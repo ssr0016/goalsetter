a3500af1bb55b960bee527a5078a3f2475d7ed6d
ssr0016 <samsonrecluta0016@gmail.com>
2024-10-09 21:45:02 +0800

    goals controller and routes setup

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 app.use('/api/goals', require('./routes/goalRoutes'))
+
+// it's override the default error handler
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
